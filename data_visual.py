31986e50edd8a6866be355b9a8bd532f60dcfdd8
Con <conolaoghaire@gmail.com>
2019-11-15 15:37:55 +0000

    Prototype graph complete
    Initial bar-chart graph complete.

--- data_visual.py
+++ data_visual.py
@@ -1,5 +1,6 @@
 import pygal
 import json
+import operator
 
 class CsStudent:
     def __init__(self, name, year, githubScore):
@@ -7,6 +8,7 @@ class CsStudent:
         self.year = year
         self.githubScore = githubScore
 csStudents = []
+amountOfYears = 5
 
 with open('Data/GithubScores|2019-11-14|14:36:29.json') as json_file:
     data = json.load(json_file)
@@ -14,17 +16,29 @@ with open('Data/GithubScores|2019-11-14|14:36:29.json') as json_file:
         csStudent = CsStudent(entry['name'],entry['year'],entry['githubScore'])
         csStudents.append(csStudent)
 
+csStudents.sort(key=lambda x: int(x.githubScore))
+#print([csStudent.name for csStudent in csStudents])
+# output: ['Leo', 'Bob', 'Alice']
 
 for csStudent in csStudents:
     print('Name: ' + csStudent.name)
     print('Year: ' + csStudent.year)
     print('GithubScore: ' + csStudent.githubScore+"\n")
 
-print(csStudents[2].githubScore)
-scores = [0] * len(csStudents)
-for i in range(len(csStudents)):
-    scores[i] = int(csStudents[i].githubScore)
+yearScores = [0] * amountOfYears
+for i in range(amountOfYears):
+    yearScores[i] = [0] * len(csStudents)
 
-bar_chart = pygal.Bar()                                            # Then create a bar graph object
-bar_chart.add('Fibonacci', scores)  # Add some values
+for i in range(len(csStudents)):
+    print("int(csStudents[i].year) = "+csStudents[i].year)
+    print("int(csStudents[i].githubScore = "+csStudents[i].githubScore)
+    yearScores[int(csStudents[i].year)][i] = int(csStudents[i].githubScore)
+
+for i in range(len(csStudents)):
+    print(str(yearScores[0][i])+" "+str(yearScores[1][i])+" "+str(yearScores[2][i])+" "+str(yearScores[3][i])+" "+str(yearScores[4][i])+"\n")
+
+bar_chart = pygal.Bar()
+bar_chart.title = 'Trinity CompSci GitHub Scores'
+bar_chart.add('3rd year', yearScores[3])
+bar_chart.add('4th year', yearScores[4])
 bar_chart.render_to_file('bar_chart.svg')
