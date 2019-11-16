86c2a6538befbe671c2d5609306a4296a272ca9e
Con <conolaoghaire@gmail.com>
2019-11-16 01:53:37 +0000

    StarCount/Followers edition

--- data_visual.py
+++ data_visual.py
@@ -3,42 +3,80 @@ import json
 import operator
 
 class CsStudent:
-    def __init__(self, name, year, githubScore):
+    def __init__(self, name, year, followers, starScore):
         self.name = name
         self.year = year
-        self.githubScore = githubScore
+        self.followers = followers
+        self.starScore = starScore
+        if(followers != 0):
+            self.ratio = float(starScore)/float(followers)
+        if(followers == 0):
+            self.ratio = 0
 csStudents = []
+amountOfStudents = 0
 amountOfYears = 5
+widthHeightScaling = 2
+yearScores = [0,0,0,0,0] # year = offset
 
-with open('Data/GithubScores|2019-11-14|14:36:29.json') as json_file:
+with open('Data/GithubScores|2019-11-16|01:19:58.json') as json_file:
     data = json.load(json_file)
     for entry in data['csStudents']:
-        csStudent = CsStudent(entry['name'],entry['year'],entry['githubScore'])
+        csStudent = CsStudent(entry['name'],entry['year'],int(entry['followers']),int(entry['starScore']))
+        if(csStudent.year == 3):
+            yearScores[3] += (csStudent.followers + csStudent.starScore)
+        if(csStudent.year == 4):
+            yearScores[4] += csStudent.followers + csStudent.starScore
         csStudents.append(csStudent)
+        amountOfStudents += 1
 
-csStudents.sort(key=lambda x: int(x.githubScore))
-#print([csStudent.name for csStudent in csStudents])
-# output: ['Leo', 'Bob', 'Alice']
-
+csStudents.sort(key=lambda x: int(x.ratio))
 for csStudent in csStudents:
     print('Name: ' + csStudent.name)
-    print('Year: ' + csStudent.year)
-    print('GithubScore: ' + csStudent.githubScore+"\n")
+    print('Year: ' + str(csStudent.year))
+    print('Followers: ' + str(csStudent.followers))
+    print("StarScore: "+str(csStudent.starScore)+"\n")
 
-yearScores = [0] * amountOfYears
+#------------BAR_CHAR-------------------------------------
+'''
+studentScores = [0] * amountOfYears
 for i in range(amountOfYears):
-    yearScores[i] = [0] * len(csStudents)
-
+    studentScores[i] = [0] * amountOfStudents
 for i in range(len(csStudents)):
-    print("int(csStudents[i].year) = "+csStudents[i].year)
-    print("int(csStudents[i].githubScore = "+csStudents[i].githubScore)
-    yearScores[int(csStudents[i].year)][i] = int(csStudents[i].githubScore)
-
-for i in range(len(csStudents)):
-    print(str(yearScores[0][i])+" "+str(yearScores[1][i])+" "+str(yearScores[2][i])+" "+str(yearScores[3][i])+" "+str(yearScores[4][i])+"\n")
+    studentScores[csStudents[i].year][i] = csStudents[i].githubScore
 
 bar_chart = pygal.Bar()
 bar_chart.title = 'Trinity CompSci GitHub Scores'
-bar_chart.add('3rd year', yearScores[3])
-bar_chart.add('4th year', yearScores[4])
+bar_chart.add('3rd year', studentScores[3])
+bar_chart.add('4th year', studentScores[4])
 bar_chart.render_to_file('bar_chart.svg')
+'''
+#----------HISTO-------------------------------------------
+def calculateWidth(y):
+    #return round((1 + y * widthHeightScaling), 2)
+    return 100 + (y * widthHeightScaling * widthHeightScaling)
+
+yearBarSets = [(0,0,0)] * amountOfYears
+for i in range(amountOfYears):
+    yearBarSets[i] = [(0,0,0)] * amountOfStudents
+
+yearBarSets[1][1] = (1,1,1)
+currentX = 0
+
+# (height, xFrom, xTo)
+for i in range(amountOfStudents):
+    student = csStudents[i]
+    ratioScore = student.ratio
+    width = calculateWidth(ratioScore)
+    #if(ratioScore == 0): ratioScore = 0.1
+    yearBarSets[int(student.year)][i] = (ratioScore, currentX, currentX + width)
+    currentX += width
+
+
+for i in range(amountOfStudents):
+    print(str(yearBarSets[0][i])+" "+str(yearBarSets[1][i])+" "+str(yearBarSets[2][i])+" "+str(yearBarSets[3][i])+" "+str(yearBarSets[4][i])+"\n")
+hist = pygal.Histogram(legend_at_bottom=True)
+hist.add('3rd year| Total score: '+str(yearScores[3]), yearBarSets[3])
+hist.add('4th year| Total score: '+str(yearScores[4]), yearBarSets[4])
+hist.title = 'GitHub StarCount/Followers: Trinity CompSci'
+hist.x_labels = ['']
+hist.render_to_file('histogram.svg')
--- extract.py
+++ extract.py
@@ -14,42 +14,36 @@ class CsStudent:
     def __init__(self, name, year):
         self.name = name
         self.year = year
-        self.githubScore = 0
+        self.followers = 0
+        self.starScore = 0
 csStudents = []
-yearScores = [0,0,0,0,0] # year = offset
 
 f = open("3rdYearCS.txt", "r")
 year3s = f.read().split()
-#f = open("4thYearCS.txt", "r")
-#year4s = f.read().split()
+f = open("4thYearCS.txt", "r")
+year4s = f.read().split()
 for username in year3s:
     csStudent = CsStudent(username,3)
     csStudents.append(csStudent)
-#for username in year4s:
-#    csStudent = CsStudent(username,4)
-#    csStudents.append(csStudent)
+for username in year4s:
+    csStudent = CsStudent(username,4)
+    csStudents.append(csStudent)
 
 g = Github("e133800e1d00c85d861492c0c8ea65e0f853d7ad")
 
 for csStudent in csStudents:
     user = g.get_user(csStudent.name)
-    csStudent.githubScore += user.followers
+    csStudent.followers += user.followers
     for repo in user.get_repos():
-        csStudent.githubScore += repo.stargazers_count
-    if(csStudent.year == 3):
-        yearScores[3] += csStudent.githubScore
-    if(csStudent.year == 4):
-        yearScores[4] += csStudent.githubScore
+        csStudent.starScore += repo.stargazers_count
     data['csStudents'].append({
         'name': csStudent.name,
         'year': str(csStudent.year),
-        'githubScore': str(csStudent.githubScore)
+        'followers': str(csStudent.followers),
+        'starScore': str(csStudent.starScore),
     })
-    print(str(csStudent.year)+": "+csStudent.name+": "+str(csStudent.githubScore))
+    print(str(csStudent.year)+": "+csStudent.name+": "+str(csStudent.followers)+": "+str(csStudent.starScore))
 f.close()
 
 with open("Data/GithubScores"+datetime+".json", "w") as outfile:
     json.dump(data, outfile)
-
-print("Year3: "+str(yearScores[3]))
-print("Year4: "+str(yearScores[4]))
